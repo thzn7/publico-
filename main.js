a7553b9f9fe0a087a7e7b6b7410d8cd037a096b5
Th da cf <127320304+thzn7@users.noreply.github.com>
2024-04-16 07:35:49 -0300

    gg

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelector(".botao")
 
 for(let i=0;i<botoes.length;i++){
 
-    botoes[i].onclick =function(){
+    botoes[i].onclick = function(){
     botoes[i].classList.add('ativo');
     }
 }
