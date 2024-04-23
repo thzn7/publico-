3abecb1d8a648ea19050491ac5d56e6a912ad5fc
Th da cf <127320304+thzn7@users.noreply.github.com>
2024-04-23 07:52:40 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const botoes = document.querySelectorAll(".botao")
+const botoes = document.QuerySelectorAlluerySelectorAll(".botao")
+const textos = document.QuerySelectorAll(".aba-conteudo")
 
 for(let i=0;i<botoes.length;i++){
     botoes[i].onclick = function(){
