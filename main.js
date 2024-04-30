4bfdbc9c946d603daccb05dc5011951327ecdee3
Th da cf <127320304+thzn7@users.noreply.github.com>
2024-04-30 07:30:24 -0300

    fff

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ for(let i=0;i<botoes.length;i++){
     botoes[i].onclick = function(){
      for(let j=0;j<botoes.length;j++){
         botoes[j].classList.remove("ativo")
+        textos[j].classList.remove("ativo")
     }
     
     
