fa1323cf685166347cf5d56c66a0b6cb3707cfa5
Th da cf <127320304+thzn7@users.noreply.github.com>
2024-04-30 07:54:37 -0300

    55

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const botoes = document.querySelectorAll(".botao")
 const textos = document.querySelectorAll(".aba-conteudo")
 const contadores = document.querySelectorAll(".contador")
-const tempoObjetivo1 = new Date("2024-10-05")
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
+let tempoAtual = new Date()
 
 for(let i=0;i<botoes.length;i++){
     botoes[i].onclick = function(){
